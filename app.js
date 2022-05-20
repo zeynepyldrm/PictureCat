d476dd88ac52a380fa5db3d4911cbb33685a3384
Zeynep Yıldırım <zeynepyldrm1516@gmail.com>
2022-05-20 23:46:31 +0300

    template engine views imported

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//template engine 
+app.set('view engine', 'ejs');
+
 //middlewares
 app.use(express.static('public'));
 
+//routes
 app.get('/', (req, res) => {
-  /* const photo={
-         id:1,
-         name:"Photo Name",
-         description:" Photo Description"
-     }
-     res.send(photo);
-     */
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+app.get('/addPhoto',(req,res)=>{
+  res.render('addPhoto');
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about');
+})
 
 const port = 3000;
 
