2b8f46fa311d51e64c4093353862fc696ee0c4f0
Zeynep Yıldırım <zeynepyldrm1516@gmail.com>
2022-05-21 13:17:09 +0300

    db setting and add photo working

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo=require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/PictureCat-Test-DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 
 //template engine 
 app.set('view engine', 'ejs');
 
 //middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //routes
 app.get('/', (req, res) => {
@@ -22,7 +33,13 @@ app.get('/addPhoto',(req,res)=>{
 
 app.get('/about',(req,res)=>{
   res.render('about');
-})
+});
+
+app.post('/photos',async(req,res)=>{
+  //console.log(req.body);
+  await Photo.create(req.body);
+  res.redirect("/");
+});
 
 const port = 3000;
 
