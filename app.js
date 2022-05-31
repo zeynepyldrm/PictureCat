668ba01ebcea3f8b421df9a25879cf30d8d9e68b
Zeynep Yıldırım <zeynepyldrm1516@gmail.com>
2022-05-31 21:47:10 +0300

    photo deleted

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var flash = require('connect-flash');
 mongoose.connect('mongodb://localhost/PictureCat-Test-DB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+ 
 });
 
 
@@ -26,7 +27,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method',{
+  methods:["POST","GET"]
+}))
 
 //routes
 app.get('/', async (req, res) => {
@@ -102,14 +105,22 @@ app.put("/photos/:id",async(req,res)=>{
   res.redirect(`/photos/${req.params.id}`)
 
 });
+app.delete("/photos/:id",async(req,res)=>{
+  const photo=await Photo.findOne({_id:req.params.id});
+  let deletedImage=__dirname+ '/public'+photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect("/");
+})
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
 
 app.get('/photos/:id', async (req, res) => {
   const photo = await Photo.findById(req.params.id);
   res.render('photo', { photo })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 const port = 3000;
